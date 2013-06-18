a866a6bf011dda41d8c131806431e77df7e672dc
BoppreH <boppreh@gmail.com>
2013-06-18 18:33:18 -0300

    Use threads on processes

--- kahn.py
+++ kahn.py
@@ -1,63 +1,51 @@
-from collections import deque
+from threading import Thread
+from queue import Queue
 
-class Kahn(object):
+class Kahn(Thread):
     def __init__(self, fn):
+        super().__init__()
         self.fn = fn
         self.in_queues = []
         self.out_queues = []
 
-    def __setitem__(self, index, queue):
-        self.out_queues[index] = queue
+    def __setitem__(self, index, q):
+        self.out_queues[index] = q
 
     def __getitem__(self, index):
         return self.in_queues[index]
 
-    def __call__(self, *inputs):
-        if not inputs:
-            inputs = [queue.popleft() for queue in self.in_queues]
+    def run(self):
+        while True:
+            inputs = [q.get(True) for q in self.in_queues]
 
-        results = self.fn(*inputs)
+            results = self.fn(*inputs)
 
-        if isinstance(results, tuple):
-            for result, queue in zip(results, self.out_queues):
-                queue.append(result)
-        elif len(self.out_queues):
-            self.out_queues[0].append(results)
-
-
-def link(producer, consumer):
-    if not isinstance(producer, Kahn):
-        producer = Kahn(producer)
-    if not isinstance(consumer, Kahn):
-        consumer = Kahn(consumer)
-        
-    queue = deque()
-    
-    producer.out_queues.append(queue)
-    consumer.in_queues.append(queue)
+            if isinstance(results, tuple):
+                for result, q in zip(results, self.out_queues):
+                    q.put(result)
+            elif len(self.out_queues):
+                self.out_queues[0].put(results)
 
 
 def connect(*connections):
+    processes = {}
     for producer, consumer in connections:
+        if not producer in processes:
+            processes[producer] = Kahn(producer)
+        if not consumer in processes:
+            processes[consumer] = Kahn(consumer)
+        
+        q = Queue()
+        processes[producer].out_queues.append(q)
+        processes[consumer].in_queues.append(q)
 
-        link(producer, consumer)
+    for process in processes.values():
+        process.start()
+
 
 if __name__ == '__main__':
-    def a():
-        return 4
+    a = lambda: 4
+    b = lambda: 5
+    add = lambda x, y: x + y
     
-    def b(n):
-        return n * 2, n
-    
-    def c(n):
-        print(n)
-    
-    def log(n):
-        print('Logged:', n)
-    
-    connect((a, b), (b, c), (b, log))
-    
-    ka()
-    kb()
-    kc()
-    klog()
+    connect((a, add), (b, add), (add, print))
